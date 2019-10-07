1ba91441319dcfdad14efd221bbd977b9bb6c4eb
Hung Hoang <hxuanhung@gmail.com>
2019-10-07 15:14:43 +0100

    use * in path for protoc

--- protoc-gen.sh
+++ protoc-gen.sh
@@ -1,31 +1,24 @@
 # message.proto
 protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example \
-  --proto_path=third_party \
+  -I $GOPATH/src \
+  -I third_party \
   --go_out=plugins=grpc:. \
-  api/pb/message.proto 
+  api/pb/*.proto 
 
-# service.proto
+# gRPC and gRPC-gateway
 protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example \
-  --proto_path=third_party \
+  -I $GOPATH/src \
+  -I third_party \
   --go_out=plugins=grpc:. \
-  api/srv/service.proto 
-
-# grpc-gateway
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example \
-  --proto_path=third_party \
   --grpc-gateway_out=logtostderr=true:. \
-  api/srv/service.proto 
+  api/srv/*.proto 
 
 # swagger
+# Note that the proto_path must be an exact prefix of the .proto file names
 protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example \
+  -I $GOPATH/src \
+  -I $GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example/api/srv \
+  -I third_party \
   --proto_path=third_party \
   --swagger_out=logtostderr=true:api/swagger \
-  api/srv/service.proto 
+  service.proto 
