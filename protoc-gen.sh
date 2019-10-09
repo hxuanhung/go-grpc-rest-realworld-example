302e817a8ffcef27f528dfd282400fb42fc712d2
Hung Hoang <hxuanhung@gmail.com>
2019-10-09 11:34:10 +0100

    change file structures

--- protoc-gen.sh
+++ protoc-gen.sh
@@ -1,24 +1,23 @@
-# message.proto
-protoc -I/usr/local/include -I. \
-  -I $GOPATH/src \
-  -I third_party \
-  --go_out=plugins=grpc:. \
-  api/pb/*.proto 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # gRPC and gRPC-gateway
+echo "Compile protobuf service definitions to Go and generate reverse-proxy..."
 protoc -I/usr/local/include -I. \
   -I $GOPATH/src \
+  -I $DIR/api/proto/v1 \
   -I third_party \
-  --go_out=plugins=grpc:. \
-  --grpc-gateway_out=logtostderr=true:. \
-  api/srv/*.proto 
+  --go_out=plugins=grpc:pkg/api/v1 \
+  --grpc-gateway_out=logtostderr=true:pkg/api/v1 \
+  protection-service.proto 
+echo "done!"
 
 # swagger
 # Note that the proto_path must be an exact prefix of the .proto file names
+echo "Generate Swagger files..."
 protoc -I/usr/local/include -I. \
   -I $GOPATH/src \
-  -I $GOPATH/src/github.com/hxuanhung/go-grpc-rest-realworld-example/api/srv \
+  -I $DIR/api/proto/v1 \
   -I third_party \
-  --proto_path=third_party \
-  --swagger_out=logtostderr=true:api/swagger \
-  service.proto 
+  --swagger_out=logtostderr=true:api/swagger/v1 \
+  protection-service.proto 
+echo "done!"
