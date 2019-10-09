5e751a28387829589db77baaf5fbd4690b9b58f8
Hung Hoang <hxuanhung@gmail.com>
2019-10-09 12:15:13 +0100

    remove third_party dir and change protoc-gen.sh to use path from $GOBIN/src/github.com

--- protoc-gen.sh
+++ protoc-gen.sh
@@ -5,7 +5,8 @@ echo "Compile protobuf service definitions to Go and generate reverse-proxy..."
 protoc -I/usr/local/include -I. \
   -I $GOPATH/src \
   -I $DIR/api/proto/v1 \
-  -I third_party \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --go_out=plugins=grpc:pkg/api/v1 \
   --grpc-gateway_out=logtostderr=true:pkg/api/v1 \
   protection-service.proto 
@@ -17,7 +18,8 @@ echo "Generate Swagger files..."
 protoc -I/usr/local/include -I. \
   -I $GOPATH/src \
   -I $DIR/api/proto/v1 \
-  -I third_party \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
+  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --swagger_out=logtostderr=true:api/swagger/v1 \
   protection-service.proto 
 echo "done!"
